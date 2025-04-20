8aaac13870277db33fefeb7fd45b59227f3cde33
kamlesh Chaudhary <chaudharykamlesh185@gmail.com>
2025-04-20 23:18:27 +0530

    Added user order list page

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,15 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from orders.models import Order
 
-class OrderListView(ListView):
+class OrderListView(LoginRequiredMixin, ListView):
     model = Order
     template_name = 'order/order_list.html'
+    context_object_name = 'orders'
 
     def get_queryset(self):
-        return Order.objects.all().prefetch_related('product')
+        # Return only the orders of the logged-in user
+        return Order.objects.filter(user=self.request.user).prefetch_related('product', 'product__images')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
