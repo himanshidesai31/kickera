422c652ca2c2b695eaf34b78db3651b0b4084fe3
himmi <himanshidesai31@gmail.com>
2026-01-18 17:31:54 +0530

    Updated Homepageview and commented old code

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.contrib import messages
+# from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
